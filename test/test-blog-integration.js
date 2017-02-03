c0f78a7a118b3ca1326bb56ff262a8d29e236f62
LarissaMorrell <larissa@larissamorrell.com>
2017-02-03 16:00:28 -0500

    POST integration test not working. GET and PUT integrations working.

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -95,7 +95,6 @@ describe('Blog App API resource', function() {
                 .then(function(_res) {
                     // so subsequent .then blocks can access resp obj.
                     res = _res;
-                    // printObj(res);
                     res.should.have.status(200);
                     // otherwise our db seeding didn't work
                     res.body.should.have.length.of.at.least(1);
@@ -127,19 +126,135 @@ describe('Blog App API resource', function() {
                     return BlogPost.findById(resPost.id);
                 })
                 .then(function(blogPost) {
-                	printObj(blogPost);
-                	// console.log(blogPost);
                     resPost.id.should.equal(blogPost._id.toString());
-                    resPost.author.should.equal(blogPost.author.firstName 
-                    	+ " " + blogPost.author.lastName);
+                    resPost.author.should.equal(
+                        blogPost.author.firstName + " " + blogPost.author.lastName);
                     resPost.content.should.equal(blogPost.content);
                     resPost.title.should.equal(blogPost.title);
-                    console.log("respost= " + resPost.created
-                    	+ "\nblogpost= " + blogPost.created);
+                    console.log("respost= " + resPost.created + "\nblogpost= " + blogPost.created);
                     Date(resPost.created).should.equal(Date(blogPost.created));
                 });
         });
     });
+
+    // describe('POST endpoint', function() {
+    //     // strategy: make a POST request with data,
+    //     // then prove that the blog post we get back has
+    //     // right keys, and that `id` is there (which means
+    //     // the data was inserted into db)
+    //     it('should add a new blog post', function() {
+
+    //         const newBlogPost = generateBlogPostData();
+    //         return chai.request(app)
+    //             .post('/posts')
+    //             .send(newBlogPost)
+    //             .then(function(res) {
+
+    //             console.log("new blogpost= ++++++++++++++++++++++++++++++++++++++++++++++" + newBlogPost.firstName);
+    //         	console.log("res blogpost= ++++++++++++++++++++++++++++++++++++++++++++++" + res.body.firstName);
+
+
+
+    //                 res.should.have.status(201);
+    //                 res.should.be.json;
+    //                 res.body.should.be.a('object');
+    //                 res.body.should.include.keys(
+    //                     'id', 'author', 'content', 'title', 'created');
+    //                 // res.body.author.should.equal(
+    //                 // 	newBlogPost.author.firstName + " " + newBlogPost.author.lastName);
+    //                 // cause Mongo should have created id on insertion
+    //                 res.body.id.should.not.be.null;
+    //                 res.body.content.should.equal(newBlogPost.content);
+    //                 res.body.title.should.equal(newBlogPost.title);
+    //                 Date(res.body.created).should.equal(Date(newBlogPost.created));
+
+    //                 return BlogPost.findById(res.body.id);
+    //             })
+    //             .then(function(blogpost) {
+    //                 blogpost.author.should.equal(newBlogPost.author);
+    //                 blogpost.cuisine.should.equal(newRestaurant.cuisine);
+    //                 blogpost.borough.should.equal(newRestaurant.borough);
+    //                 blogpost.name.should.equal(newRestaurant.name);
+    //                 blogpost.grade.should.equal(mostRecentGrade);
+    //                 blogpost.address.building.should.equal(newRestaurant.address.building);
+    //                 blogpost.address.street.should.equal(newRestaurant.address.street);
+    //                 blogpost.address.zipcode.should.equal(newRestaurant.address.zipcode);
+    //             });
+    //     });
+    // });
+
+
+    describe('PUT endpoint', function() {
+
+        // strategy:
+        //  1. Get an existing blog post from db
+        //  2. Make a PUT request to update that blog post
+        //  3. Prove blog post returned by request contains data we sent
+        //  4. Prove blog post in db is correctly updated
+        it('should update fields you send over', function() {
+            const updateData = {
+                author: {
+                    firstName: "Callie",
+                    lastName: "Walsh"
+                },
+                title: "My new title is right here",
+            };
+
+            return BlogPost
+                .findOne()
+                .exec()
+                .then(function(blogPost) {
+                    updateData.id = blogPost.id;
+
+                    // make request then inspect it to make sure it reflects
+                    // data we sent
+                    return chai.request(app)
+                        .put(`/posts/${blogPost.id}`)
+                        .send(updateData);
+                })
+                .then(function(res) {
+                    // res.should.have.status(204);
+
+                    return BlogPost.findById(updateData.id).exec();
+                })
+                .then(function(blogPost) {
+                    blogPost.author.firstName.should.equal(updateData.author.firstName);
+                    blogPost.author.lastName.should.equal(updateData.author.lastName);
+                    blogPost.title.should.equal(updateData.title);
+                });
+        });
+    });
+
+    // describe('DELETE endpoint', function() {
+    //   // strategy:
+    //   //  1. get a restaurant
+    //   //  2. make a DELETE request for that restaurant's id
+    //   //  3. assert that response has right status code
+    //   //  4. prove that restaurant with the id doesn't exist in db anymore
+    //   it('delete a restaurant by id', function() {
+
+    //     let restaurant;
+
+    //     return Restaurant
+    //       .findOne()
+    //       .exec()
+    //       .then(function(_restaurant) {
+    //         restaurant = _restaurant;
+    //         return chai.request(app).delete(`/restaurants/${restaurant.id}`);
+    //       })
+    //       .then(function(res) {
+    //         res.should.have.status(204);
+    //         return Restaurant.findById(restaurant.id).exec();
+    //       })
+    //       .then(function(_restaurant) {
+    //         // when a variable's value is null, chaining `should`
+    //         // doesn't work. so `_restaurant.should.be.null` would raise
+    //         // an error. `should.be.null(_restaurant)` is how we can
+    //         // make assertions about a null value.
+    //         should.not.exist(_restaurant);
+    //       });
+    //   });
+    // });
 
     function printObj(res) {
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
